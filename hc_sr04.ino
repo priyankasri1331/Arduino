08deb0e711906b844faaa8304d22cd8e27a82d8c
Priyanka K <priyankasri1331@gmail.com>
2023-12-24 22:56:42 -0500

    Update hc_sr04.ino

--- hc_sr04.ino
+++ hc_sr04.ino
@@ -1,9 +1,9 @@
-/*
+or/*
 
 */
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-// this constant won't change. It's the pin number of the sensor's output:
+//This constant won't change. It's the PIN of the sensor's output:
 const int TrigPin = 7;
 const int echoPin = 8;
 int display_delay = 0;
@@ -21,14 +21,14 @@ void setup() {
 }
 
 void loop() {
-  // establish variables for duration of the ping, and the distance result
+  // establish variables for the duration of the ping, and the distance result
   // in inches and centimeters:
 
   display_delay++;
   
   long duration, inches, cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
 
   digitalWrite(TrigPin, LOW);
@@ -37,10 +37,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TrigPin, LOW);
 
-  // The same pin is used to read the signal from the PING))): a HIGH pulse
-  // whose duration is the time (in microseconds) from the sending of the ping
-  // to the reception of its echo off of an object.
-
+  //sensor reading
   duration = pulseIn(echoPin, HIGH);
 
   // convert the time into a distance
@@ -53,6 +50,8 @@ void loop() {
   Serial.print("cm");
   Serial.println();
   Serial.println(duration);
+
+  //More delay for LCD display
 
   if(display_delay >=10) 
   {
@@ -69,7 +68,7 @@ void loop() {
 long microsecondsToInches(long microseconds) {
   // According to Parallax's datasheet for the PING))), there are 73.746
   // microseconds per inch (i.e. sound travels at 1130 feet per second).
-  // This gives the distance travelled by the ping, outbound and return,
+  // This gives the distance traveled by the ping, outbound and return,
   // so we divide by 2 to get the distance of the obstacle.
   // See: https://www.parallax.com/package/ping-ultrasonic-distance-sensor-downloads/
   return microseconds / 74 / 2;
@@ -78,6 +77,6 @@ long microsecondsToInches(long microseconds) {
 long microsecondsToCentimeters(long microseconds) {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
+  //Take half of the distance traveled.
   return microseconds / 29 / 2;
 }
